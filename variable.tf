2d5d5cfe83dc9ab16df3c1dafae305a0364c4626
Olufemi Peter Onafuye <162372970+pharmonny@users.noreply.github.com>
2024-11-06 22:28:50 -0500

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "bucket_name" {
+    default = "pharmotfbct"
+  
+}
+variable "environment_name" {
+    default = "dev"
+  
+}
+
+variable "bucket_name" {
+    default = "pharmotfbct-devops"
+  
+}
+variable "environment_name" {
+    default = "dev"
+  
+}
+variable "ec2_ami" {
+    default = "ami-0b8b44ec9a8f90422"
+  
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "subnet_id" {
+    default = "subnet-0c2fee1e3d5f890e0"
+  
+}
